b1418801aad369369cb10b2c31d64844d735d757
josh long <josh@sonr.io>
2022-03-31 10:19:55 -0400

    ref: fix to png file naming

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 const imageStore: Map<number, Uint8Array> = new Map();
 const uri = `https://larvalabs.com/public/images/cryptopunks/punk`;
+const dir = './punks';
 const range = 1000;
-
-console.log(`Starting scrape of gallery: ${uri}`);
 
 function convertInt(i: number): string {
     if (`${i}`.length < 1) return '0000';
     let value = '';
+    // hacky stuff for numerical conversion
     for (let i = 0; i < 4 - `${i}`.length; i++) {
         value += '0';
     }
@@ -20,6 +20,9 @@ function cb(stream: ReadableStreamReadResult<Uint8Array>, i: number): void {
     imageStore.set(i, stream.value ? stream.value : new Uint8Array(353));
 }
 
+await Deno.mkdir('./punks', { mode: 0o700 }).catch(() => { console.log('Directory already exists' )});
+console.log(`Starting scrape of gallery: ${uri}`);
+
 for (let i = 0; i < range; i++) {
     const convertedNumeric: string = convertInt(i);
     const resp: Response = await fetch(uri + convertedNumeric + '.png');
@@ -27,13 +30,11 @@ for (let i = 0; i < range; i++) {
         cb(stream, i);
     });
 }
-
-await Deno.mkdir('./punks', { mode: 0o700 }).catch(() => { console.log('Directory already exists' )});
 for (const key of imageStore.keys()) {
     try
     {
         if (imageStore.get(key))
-            Deno.writeFileSync(`./../punks/png-${key}.png`, imageStore.get(key) as Uint8Array);
+            Deno.writeFileSync(`./${dir}/punk-${key}.png`, imageStore.get(key) as Uint8Array);
     } catch (e) {
         console.log(e.message);
     }
